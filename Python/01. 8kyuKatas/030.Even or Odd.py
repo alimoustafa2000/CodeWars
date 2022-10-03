5c1d01ab9034274f70b4df399069f1c38e1b7abd
Ali Moustafa <103620928+alimoustafa2000@users.noreply.github.com>
2022-10-03 17:01:04 +0200

    new solution

---
+++ Python/01. 8kyuKatas/030.Even or Odd.py
@@ -0,0 +1,17 @@
+def even_or_odd(number):
+
+    x = number % 2
+
+    if x == 0:
+
+        return "Even"
+
+    else:
+
+        return "Odd"
+
+print(even_or_odd(10))
+#Even
+
+print(even_or_odd(15))
+#Odd
